086ff85ef69305c398726864eb974064fef5b19d
tjtly <266075057@qq.com>
2019-04-09 14:00:50 +0800

    首页

--- src/main/java/net/lightwing/mediweb/controller/MainController.java
+++ src/main/java/net/lightwing/mediweb/controller/MainController.java
@@ -2,9 +2,11 @@ package net.lightwing.mediweb.controller;
 
 import net.lightwing.mediweb.commons.Constant;
 import net.lightwing.mediweb.commons.HtmlToText;
+import net.lightwing.mediweb.pojo.MCoop;
 import net.lightwing.mediweb.pojo.MNews;
 import net.lightwing.mediweb.service.MBannerService;
 import net.lightwing.mediweb.service.MConfigService;
+import net.lightwing.mediweb.service.MCoopService;
 import net.lightwing.mediweb.service.MNewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +18,7 @@ import java.util.List;
 
 @SuppressWarnings("ALL")
 @Controller
-public class MainController
-{
+public class MainController {
     @Autowired
     private MConfigService configService;
     @Autowired
@@ -25,25 +26,26 @@ public class MainController
     @Autowired
     private MNewsService newsService;
 
-    @RequestMapping(path = {"index.html","/"})
-    public String Main(Model model)
-    {
-        model.addAttribute("config",configService.getConfig(Constant.CONFIGID));
-        model.addAttribute("banners",bannerService.getList(1,6));
-        List<MNews> list = newsService.getList(1,3);
-        for (MNews data:list)
-        {
+    @Autowired
+    private MCoopService mCoopService;
+
+    @RequestMapping(path = {"index.html", "/"})
+    public String Main(Model model) {
+        model.addAttribute("config", configService.getConfig(Constant.CONFIGID));
+        model.addAttribute("banners", bannerService.getList(1, 6));
+        List<MNews> list = newsService.getList(1, 3);
+        for (MNews data : list) {
             data.setNdetail(HtmlToText.Html2Text(data.getNdetail()));
         }
-        model.addAttribute("news",list);
+        model.addAttribute("news", list);
 
-        List<MNews> bottomNews = newsService.getList(1,2);
-        for (MNews data:bottomNews)
-        {
+        List<MNews> bottomNews = newsService.getList(1, 2);
+        for (MNews data : bottomNews) {
             data.setNdetail(HtmlToText.Html2Text(data.getNdetail()));
         }
-        model.addAttribute("bottomNews",bottomNews);
-
+        List<MCoop> mCoopList = mCoopService.getList(1, 6);
+        model.addAttribute("bottomNews", bottomNews);
+        model.addAttribute("mCoopList", mCoopList);
         return "index.html";
     }
 }
